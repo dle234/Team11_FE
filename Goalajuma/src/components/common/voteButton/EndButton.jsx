f3417da125b4c8eeb250e677cd3a0bcb0e9d6108
GimGayeon <kky1373@naver.com>
2023-11-09 15:26:24 +0900

    feat: 배포

--- Goalajuma/src/components/common/voteButton/EndButton.jsx
+++ Goalajuma/src/components/common/voteButton/EndButton.jsx
@@ -7,10 +7,10 @@ import Swal from "sweetalert2";
 
 /**
  * @param {object} props
- * @param {boolean} props.isOwner 작성자 확인
+ * @param {boolean} props.isOwner 작성자 확인 
  * @param {string} props.active 투표 진행중 여부 : continue, complete
  */
-const EndButton = ({ isOwner, id, active: initialActive }) => {
+const EndButton = ({isOwner, id, active: initialActive}) => {
   const [active, setActive] = useState(initialActive);
   const [alert, setIsAlert] = useState(false);
 
@@ -23,33 +23,34 @@ const EndButton = ({ isOwner, id, active: initialActive }) => {
       cancelButtonText: "아니오",
       confirmButtonColor: "#429f50",
       cancelButtonColor: "#d33",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // window.location.reload();
-        setActive("complete");
-        setIsAlert(true);
-        closeInquire(id);
+    }).then((result)=>{
+      if(result.isConfirmed){
         window.location.reload();
+        setActive("complete")
+        setIsAlert(true)
+        closeInquire(id);
       }
-    });
+    })
   };
 
   return (
     <>
-      {alert && <Alert setIsAlert={setIsAlert}>투표가 종료됩니다.</Alert>}
-      {isOwner && active === "continue" ? (
-        <ButtonStyled onClick={handleOnClick}>끝내기</ButtonStyled>
-      ) : (
-        ""
-      )}
+      {alert && 
+      <Alert setIsAlert={setIsAlert}>
+        투표가 종료됩니다.
+      </Alert>}
+      {isOwner && active === "continue"? 
+      <ButtonStyled onClick={handleOnClick}>끝내기</ButtonStyled>
+      :""}
     </>
-  );
-};
+  )
+
+}
 EndButton.propTypes = {
   isOwner: PropTypes.bool.isRequired,
   id: PropTypes.number.isRequired,
-  active: PropTypes.string.isRequired,
-};
+  active: PropTypes.string.isRequired
+}
 const ButtonStyled = styled.button`
   width: 60px;
   height: 6px;
@@ -57,7 +58,7 @@ const ButtonStyled = styled.button`
   justify-content: center;
   align-items: center;
   padding: 15px 12px;
-  background: #ff8e8e;
+  background: #FF8E8E;
   font-weight: 700;
   font-size: 10px;
   border-radius: 20px;
@@ -67,9 +68,9 @@ const ButtonStyled = styled.button`
   line-height: 15px;
   position: relative;
   top: 2px;
-  &:hover {
-    background-color: #d47575;
+  &:hover{
+    background-color: #D47575;
     cursor: pointer;
   }
-`;
-export default EndButton;
+`
+export default EndButton
